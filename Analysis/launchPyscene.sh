03d48d4b1c49fcf63809a2e020bbff48d2c55133
steveamougou <amougou.s.pp@gmail.com>
2022-06-16 11:59:24 -0400

    modified bash script to have correct input path

--- Analysis/launchPyscene.sh
+++ Analysis/launchPyscene.sh
@@ -3,26 +3,34 @@ source ~/Venvs/Pyscene/bin/activate
 
 cd ~/friends_annotations/PySceneDetect
 
-for file in ~/data/neuromod/DATA/cneuromod/friends/stimuli/s1/*.mkv; do
+import os
+
+for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s1");
+    if file.endswith(".mkv");
     python3 -m scenedetect -c Config/s1.cfg --input file detect-adaptive list-scenes
 done
 
-for file in ~/data/neuromod/DATA/cneuromod/friends/stimuli/s2/*.mkv; do
+for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s2");
+    if file.endswith(".mkv");
     python3 -m scenedetect -c Config/s2.cfg --input file detect-adaptive list-scenes
 done
 
-for file in ~/data/neuromod/DATA/cneuromod/friends/stimuli/s3/*.mkv; do
+for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s3");
+    if file.endswith(".mkv");
     python3 -m scenedetect -c Config/s3.cfg --input file detect-adaptive list-scenes
 done
 
-for file in ~/data/neuromod/DATA/cneuromod/friends/stimuli/s4/*.mkv; do
+for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s4");
+    if file.endswith(".mkv");
     python3 -m scenedetect -c Config/s4.cfg --input file detect-adaptive list-scenes
 done
 
-for file in ~/data/neuromod/DATA/cneuromod/friends/stimuli/s5/*.mkv; do
+for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s5");
+    if file.endswith(".mkv");
     python3 -m scenedetect -c Config/s5.cfg --input file detect-adaptive list-scenes
 done
 
-for file in ~/data/neuromod/DATA/cneuromod/friends/stimuli/s6/*.mkv; do
+for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s6");
+    if file.endswith(".mkv");
     python3 -m scenedetect -c Config/s6.cfg --input file detect-adaptive list-scenes
 done
