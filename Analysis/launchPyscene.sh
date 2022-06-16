01ed00a0cb51e36c9253426bb9d55e3bf8ad6814
steveamougou <amougou.s.pp@gmail.com>
2022-06-16 13:56:03 -0400

    modified bash script to have correct input path

--- Analysis/launchPyscene.sh
+++ Analysis/launchPyscene.sh
@@ -3,34 +3,69 @@ source ~/Venvs/Pyscene/bin/activate
 
 cd ~/friends_annotations/PySceneDetect
 
-import os
 
-for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s1");
-    if file.endswith(".mkv");
+for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s1/*.mkv; do
     python3 -m scenedetect -c Config/s1.cfg --input file detect-adaptive list-scenes
 done
 
-for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s2");
-    if file.endswith(".mkv");
+for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s2; do
     python3 -m scenedetect -c Config/s2.cfg --input file detect-adaptive list-scenes
 done
 
-for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s3");
-    if file.endswith(".mkv");
+for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s3; do
     python3 -m scenedetect -c Config/s3.cfg --input file detect-adaptive list-scenes
 done
 
-for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s4");
-    if file.endswith(".mkv");
+for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s4; do
     python3 -m scenedetect -c Config/s4.cfg --input file detect-adaptive list-scenes
 done
 
-for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s5");
-    if file.endswith(".mkv");
+for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s5; do
     python3 -m scenedetect -c Config/s5.cfg --input file detect-adaptive list-scenes
 done
 
-for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s6");
-    if file.endswith(".mkv");
+for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s6; do
     python3 -m scenedetect -c Config/s6.cfg --input file detect-adaptive list-scenes
 done
+
+
+
+
+
+#
+# #source /path/to/virtualenv/bin/activate
+# source ~/Venvs/Pyscene/bin/activate
+#
+# cd ~/friends_annotations/PySceneDetect
+#
+# import os
+#
+# for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s1");
+#     if file.endswith(".mkv");
+#     python3 -m scenedetect -c Config/s1.cfg --input file detect-adaptive list-scenes
+# done
+#
+# for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s2");
+#     if file.endswith(".mkv");
+#     python3 -m scenedetect -c Config/s2.cfg --input file detect-adaptive list-scenes
+# done
+#
+# for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s3");
+#     if file.endswith(".mkv");
+#     python3 -m scenedetect -c Config/s3.cfg --input file detect-adaptive list-scenes
+# done
+#
+# for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s4");
+#     if file.endswith(".mkv");
+#     python3 -m scenedetect -c Config/s4.cfg --input file detect-adaptive list-scenes
+# done
+#
+# for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s5");
+#     if file.endswith(".mkv");
+#     python3 -m scenedetect -c Config/s5.cfg --input file detect-adaptive list-scenes
+# done
+#
+# for file in os.listdir("~/data/neuromod/DATA/cneuromod/friends/stimuli/s6");
+#     if file.endswith(".mkv");
+#     python3 -m scenedetect -c Config/s6.cfg --input file detect-adaptive list-scenes
+# done
