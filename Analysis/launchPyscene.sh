85603324e4dbbf7ab11b82440850fb8ffade17ac
steveamougou <amougou.s.pp@gmail.com>
2022-06-16 14:30:51 -0400

    correct bash sript path

--- Analysis/launchPyscene.sh
+++ Analysis/launchPyscene.sh
@@ -3,30 +3,48 @@ source ~/Venvs/Pyscene/bin/activate
 
 cd ~/friends_annotations/PySceneDetect
 
+season = ${1}
 
-for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s1/*.mkv; do
-    python3 -m scenedetect -c Config/s1.cfg --input file detect-adaptive list-scenes
+for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s${season}/friends_s0*e*b.mkv.mkv; do
+    python3 -m scenedetect -c Config/s${season}.cfg --input "$file" detect-adaptive list-scenes
 done
 
-for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s2; do
-    python3 -m scenedetect -c Config/s2.cfg --input file detect-adaptive list-scenes
-done
 
-for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s3; do
-    python3 -m scenedetect -c Config/s3.cfg --input file detect-adaptive list-scenes
-done
 
-for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s4; do
-    python3 -m scenedetect -c Config/s4.cfg --input file detect-adaptive list-scenes
-done
 
-for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s5; do
-    python3 -m scenedetect -c Config/s5.cfg --input file detect-adaptive list-scenes
-done
 
-for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s6; do
-    python3 -m scenedetect -c Config/s6.cfg --input file detect-adaptive list-scenes
-done
+
+
+
+
+
+
+
+
+
+
+
+#UNUSED
+# for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s2/friends_s0*e*b.mkv.mkv; do
+#     python3 -m scenedetect -c Config/s2.cfg --input "$file" detect-adaptive list-scenes
+# done
+#
+# for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s3/friends_s0*e*b.mkv.mkv; do
+#     python3 -m scenedetect -c Config/s3.cfg --input "$file" detect-adaptive list-scenes
+# done
+#
+# for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s4/friends_s0*e*b.mkv.mkv; do
+#     python3 -m scenedetect -c Config/s4.cfg --input "$file" detect-adaptive list-scenes
+# done
+#
+# for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s5/friends_s0*e*b.mkv.mkv; do
+#     python3 -m scenedetect -c Config/s5.cfg --input "$file" detect-adaptive list-scenes
+# done
+#
+# for file in /data/neuromod/DATA/cneuromod/friends/stimuli/s6/friends_s0*e*b.mkv.mkv; do
+#     python3 -m scenedetect -c Config/s6.cfg --input "$file" detect-adaptive list-scenes
+# done
+
 
 
 
