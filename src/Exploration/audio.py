b5971bedf80cafb33334bc131d37fda3453034a0
steveamougou <amougou.s.pp@gmail.com>
2022-06-22 17:36:01 -0400

    testing audio script again

--- src/Exploration/audio.py
+++ src/Exploration/audio.py
@@ -4,6 +4,6 @@ import glob
 files = sorted(glob.glob(r"/data/neuromod/DATA/cneuromod/friends/stimuli/s*/*.mkv"))
 
 for file in files:
-    print(file[-19:])
+    # print(file[-19:])
     if "a" in file[-19:] or "b" in file[-19:]:
          print(file)
