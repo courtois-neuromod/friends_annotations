4bb30ee0d93940813a7a9e2dfed809767a8d4cd2
steveamougou <amougou.s.pp@gmail.com>
2022-06-22 17:41:29 -0400

    script audio final prete a etre run

--- src/Exploration/audio.py
+++ src/Exploration/audio.py
@@ -6,4 +6,9 @@ files = sorted(glob.glob(r"/data/neuromod/DATA/cneuromod/friends/stimuli/s*/*.mk
 for file in files:
     # print(file[-19:])
     if "a" in file[-19:] or "b" in file[-19:]:
-         print(file)
+         # print(file)
+        name = file[-18:]
+        name = name.replace(".mkv", "")
+#         print(name)
+        my_clip = mp.VideoFileClip(r""+file)
+        my_clip.audio.write_audiofile(r"/home/samougou/friends_annotations/data/audiofiles/"+name+".wav")
