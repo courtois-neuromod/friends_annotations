58497d7ca10f94f615608db00b631725efd561e5
steveamougou <amougou.s.pp@gmail.com>
2022-07-04 14:47:05 -0400

    file to add apeakers

--- Analysis/text_to_transcript.sh
+++ Analysis/text_to_transcript.sh
@@ -4,5 +4,5 @@ cd /home/steveb/GitHub/friends_annotations/src/Corpus/
 for file in /home/steveb/GitHub/friends_annotations/results/Texts/*.txt; do
   name=${file:54:6}
   #echo $name
-  python3 text_to_transcript.py  ${file} --output-file /home/steveb/GitHub/friends_annotations/results/Transcripts/${name}.json
+  python3 text_to_transcript.py  ${file} --speaker-folder /home/steveb/GitHub/friends_annotations/results/corpusEm/ --output-file /home/steveb/GitHub/friends_annotations/results/speakerTR/${name}.json
 done
